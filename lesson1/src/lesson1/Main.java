28d6bef4a57bae9949c3d2dda141ae37685317cb
berlinisimus <berlinisimus.92@gmail.com>
2015-10-11 10:48:17 +0300

    Added Main Class file

---
+++ lesson1/src/lesson1/Main.java
@@ -0,0 +1,21 @@
+package lesson1;
+
+/**
+ * To check task #1 from HomeWork uncomment "Factorial worker"
+ * To check task #2 from HomeWork uncomment "Fibonacci worker"
+ * To check task #3 from HomeWork uncomment "Converter worker"
+ */
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+       /* Factorial worker = new Factorial();
+        worker.findFactorial();*/
+
+        /*Fibonacci worker = new Fibonacci();
+        worker.printFibonacciSequence();*/
+
+        /*Converter worker = new Converter();
+        worker.convertDigitToWord();*/
+    }
+}
