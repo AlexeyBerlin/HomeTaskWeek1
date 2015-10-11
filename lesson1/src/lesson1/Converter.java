b0c6329b207e2351b1bfa386522518104d5c5741
berlinisimus <berlinisimus.92@gmail.com>
2015-10-11 10:47:29 +0300

    Added completed task #3

---
+++ lesson1/src/lesson1/Converter.java
@@ -0,0 +1,25 @@
+package lesson1;
+
+import java.util.Scanner;
+
+/**
+ * Method convertDigitToWord is used to convert a number to it's word representation
+ */
+
+public class Converter {
+
+    public void convertDigitToWord() {
+        int digit;
+        String arrayOfWords[] = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please, enter digit to convert");
+        digit = input.nextInt();
+
+        if(digit < 0 || digit > 9){
+            System.out.print("Sorry, but You should enter digit >= 0 and <= 9");
+        }else{
+            System.out.print(digit + " -> " + arrayOfWords[digit]);
+        }
+    }
+}
